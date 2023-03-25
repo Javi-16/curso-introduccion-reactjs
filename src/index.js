7949b41c267ff46436356cd8052dd629264a1521
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-03-25 19:49:32 -0400

    Intento arreglar el push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+/* import './index.css'; */
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App />,
+    /*<App>
+        Este texto se guarda en una propiedad especial llamada children
+    </App>*/
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+ReactDOM.render(
+    <App message="Learning React" />,
+    document.getElementById('root')
+)
+*/
