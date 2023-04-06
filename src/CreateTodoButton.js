0734b220e468bb22fa575ba662c2913c5e100714
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-04-06 15:13:21 -0400

    Agrego eventos de click y modificación de la barra de tareas

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton() {
+    const onClickButton = () => {
+        alert('Aquí se debería abrir el modal');
+    }
+
     return(
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className='CreateTodoButton'
+            onClick={onClickButton}
+        >
+            +
+        </button>
     );
 }
 
