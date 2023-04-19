0cc6dc45f79f06f31332e3a309bc91b6b4d354da
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-04-18 22:55:23 -0400

    Agregamos funcionalidad a la barra buscadora utilizando estados

--- src/App.js
+++ src/App.js
@@ -6,29 +6,58 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 /* import './App.css'; */
 
-const todos = [
-  { text: 'Cortar cebolla', completed: false },
-  { text: 'Tomar el curso de intro a React', completed: true },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALA', completed: false }
+const defaultTodos = [
+    { text: 'Cortar cebolla', completed: false },
+    { text: 'Tomar el curso de intro a React', completed: true },
+    { text: 'Llorar con la llorona', completed: false },
+    { text: 'LALALALA', completed: false }
 ]
 
 function App() {
-  return (
-    <React.Fragment>  
-      <TodoCounter />
+    const [todos, setTodos] = React.useState(defaultTodos);
+    const [searchValue, setSearchValue] = React.useState('');
 
-      <TodoSearch />
+    const completedTodos = todos.filter(todo => !!todo.completed).length;
+    const totalTodos = todos.length;
 
-      <TodoList>
-        {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
-        ))}
-      </TodoList>
+    let searchedTodos = [];
 
-      <CreateTodoButton />
-    </React.Fragment>
-  );
+    if (!searchValue.length >= 1) {
+        searchedTodos = todos;
+    }
+    else {
+        searchedTodos = todos.filter(todo => {
+            const todoText = todo.text.toLowerCase();
+            const searchText = searchValue.toLowerCase();
+            return todoText.includes(searchText);
+        })
+    }
+
+    return (
+        <React.Fragment>  
+            <TodoCounter 
+                total = {totalTodos}
+                completed = {completedTodos}
+            />
+
+            <TodoSearch 
+                searchValue = {searchValue}
+                setSearchValue = {setSearchValue}
+            />
+
+            <TodoList>
+            {searchedTodos.map(todo => (
+                <TodoItem 
+                    key={todo.text} 
+                    text={todo.text} 
+                    completed={todo.completed} 
+                />
+            ))}
+            </TodoList>
+
+            <CreateTodoButton />
+        </React.Fragment>
+    );
 }
 
 export default App;
