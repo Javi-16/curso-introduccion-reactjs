6ac21166a0e7a56461b4bbece74dfa09f18e233c
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-04-21 13:54:31 -0400

    Agrego el local Storage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-/* import './App.css'; */
 
-const defaultTodos = [
+/* const defaultTodos = [
     { text: 'Cortar cebolla', completed: false },
     { text: 'Tomar el curso de intro a React', completed: true },
     { text: 'Llorar con la llorona', completed: false },
     { text: 'LALALALA', completed: false }
-]
+] */
 
 function App() {
-    const [todos, setTodos] = React.useState(defaultTodos);
+    const localStorageTodos = localStorage.getItem('TODOS_V1');
+    let parsedTodos;
+
+    if (!localStorageTodos) {
+        localStorage.setItem('TODOS_V1', JSON.stringify([]));
+        parsedTodos = [];
+    }
+    else {
+        parsedTodos = JSON.parse(localStorageTodos);
+    }
+
+    const [todos, setTodos] = React.useState(parsedTodos);
     const [searchValue, setSearchValue] = React.useState('');
 
     const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -33,11 +43,17 @@ function App() {
         })
     }
 
+    const saveTodos = (newTodos)  => {
+        const stringifiedTodos = JSON.stringify(newTodos);
+        localStorage.setItem('TODOS_V1', stringifiedTodos);
+        setTodos(newTodos);
+    }
+
     const completeTodos = (text) => {
         const todoIndex = todos.findIndex(todo => todo.text === text);
         const newTodos = [...todos];
         newTodos[todoIndex].completed = true;
-        setTodos(newTodos);
+        saveTodos(newTodos);
     }
 
     const deleteTodos = (text) => {
